c6bb1a6ad905ae9d400e6db2fa8d4eecca80e617
TuanKhanh-Ng <ntklxag@gmail.com>
2023-07-24 19:18:25 +0700

    feat(ui): design the layout of Options.kt which is used in Details.kt

--- app/src/main/java/com/ngtuankhanh/android/coffeehouse/feature/home/details/Details.kt
+++ app/src/main/java/com/ngtuankhanh/android/coffeehouse/feature/home/details/Details.kt
@@ -1,92 +1,166 @@
 package com.ngtuankhanh.android.coffeehouse.feature.home.details
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ngtuankhanh.android.coffeehouse.R
 import com.ngtuankhanh.android.coffeehouse.ui.theme.CoffeeHouseTheme
+import com.ngtuankhanh.android.coffeehouse.ui.theme.poppinsFamily
+import com.ngtuankhanh.android.coffeehouse.ui.theme.typography
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Details() {
+fun Details(coffeeName: String) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "Details") },
+                title = {
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Details",
+                            style = typography.titleMedium,
+                            color = Color(0xFF001833)
+                        )
+                    }
+                },
                 navigationIcon = {
                     IconButton(onClick = {}) {
                         Icon(
-                            Icons.Default.ArrowBack, contentDescription = null
+                            Icons.Default.ArrowBack,
+                            contentDescription = null,
+                            tint = Color(0xFF001833)
                         )
                     }
                 },
                 actions = {
                     IconButton(onClick = {}) {
                         Icon(
-                            painterResource(id = R.drawable.buy), contentDescription = null
+                            painterResource(id = R.drawable.buy),
+                            contentDescription = null,
+                            tint = Color(0xFF001833)
                         )
                     }
                 })
         },
         modifier = Modifier.padding(24.dp)
     ) { contentPadding ->
-        Column(
-            verticalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .padding(contentPadding)
-                .fillMaxSize()
-        ) {
-            Column() {
-                Image(
-                    painterResource(id = R.drawable.americano),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                AmountOption(name = "Americano")
-                ShotOption()
-                HotColdOption()
-                SizeOption()
-                IceOption()
-            }
-            Column() {
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(text = "Total Amount")
-                    Text(text = "$3.00")
+        Surface(modifier = Modifier.padding(contentPadding)) {
+            val totalAmount = rememberSaveable { mutableStateOf(0.0) }
+            val counter = rememberSaveable { mutableStateOf(1) }
+            Column(
+                verticalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .padding(start = 24.dp, end = 24.dp, bottom = 24.dp)
+                    .fillMaxSize()
+            ) {
+                Column() {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(150.dp)
+                    ) {
+                        Image(
+                            painterResource(id = R.drawable.americano),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+                    AmountOption(name = coffeeName, counter = counter)
+                    ShotOption()
+                    HotColdOption()
+                    SizeOption()
+                    IceOption()
                 }
-                Button(onClick = {}, modifier = Modifier.fillMaxWidth()) {
-                    Text(text = "Add to cart")
+                Column() {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 24.dp)
+                    ) {
+                        Text(
+                            text = "Total Amount",
+                            fontFamily = poppinsFamily,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            letterSpacing = 0.sp,
+                            color = Color(0xFF001833)
+                        )
+                        Text(
+                            text = "$${totalAmount.value}",
+                            fontFamily = poppinsFamily,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            letterSpacing = 0.sp,
+                            color = Color(0xFF001833)
+                        )
+                    }
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .clip(shape = RoundedCornerShape(30.dp))
+                            .fillMaxWidth()
+                            .clickable(onClick = {
+
+                            })
+                            .background(Color(0xFF324A59))
+                            .padding(12.dp)
+                    ) {
+                        Text(
+                            text = "Add to cart",
+                            style = typography.titleSmall,
+                            color = Color(0xFFFFFFFF)
+                        )
+                    }
                 }
             }
         }
     }
+
 }
 
 @Preview
 @Composable
 fun DetailsPreview() {
     CoffeeHouseTheme() {
-        Details()
+        Details("Americano")
     }
 }
--- app/src/main/java/com/ngtuankhanh/android/coffeehouse/feature/home/details/Options.kt
+++ app/src/main/java/com/ngtuankhanh/android/coffeehouse/feature/home/details/Options.kt
@@ -1,140 +1,323 @@
 package com.ngtuankhanh.android.coffeehouse.feature.home.details
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ngtuankhanh.android.coffeehouse.R
 import com.ngtuankhanh.android.coffeehouse.ui.theme.CoffeeHouseTheme
+import com.ngtuankhanh.android.coffeehouse.ui.theme.typography
 
 @Composable
-fun AmountOption(name: String) {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Row(
-            modifier = Modifier.padding(
+fun AmountOption(name: String, counter: MutableState<Int>) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
                 top = 16.dp,
                 bottom = 16.dp,
                 start = 8.dp,
                 end = 8.dp
             )
+    ) {
+        Text(text = name, style = typography.titleSmall, color = Color(0xFF001833))
+        Row(
+            modifier = Modifier
+                .width(80.dp)
+                .height(30.dp)
+                .border(
+                    width = 2.dp,
+                    color = Color(0xFFD8D8D8),
+                    shape = RoundedCornerShape(percent = 50)
+                )
         ) {
-            Text(text = name)
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .clickable(onClick = {
+                        ++counter.value
+                    })
+                    .weight(1f)
+                    .fillMaxSize()
+            ) {
+
+                Text(
+                    text = "+",
+                    textAlign = TextAlign.Center,
+                    style = typography.titleSmall,
+                    color = Color(0xFF001833)
+                )
+            }
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+            ) {
+
+                Text(
+                    text = "${counter.value}",
+                    textAlign = TextAlign.Center,
+                    style = typography.titleSmall,
+                    color = Color(0xFF001833)
+                )
+            }
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .clickable(onClick = {
+                        if (counter.value > 1) --counter.value
+                    })
+                    .weight(1f)
+                    .fillMaxSize()
+            ) {
+
+                Text(
+                    text = "-",
+                    textAlign = TextAlign.Center,
+                    style = typography.titleSmall,
+                    color = Color(0xFF001833)
+                )
+            }
         }
-        Divider(
-            color = Color.Cyan, modifier = Modifier
-                .height(1.dp)
-                .fillMaxWidth()
-        )
     }
 }
 
 @Composable
 fun ShotOption() {
     Divider(
-        color = Color.Cyan, modifier = Modifier
-            .height(1.dp)
+        color = Color(0xFFD8D8D8), modifier = Modifier
+            .height(2.dp)
             .fillMaxWidth()
     )
     Row(
-        modifier = Modifier.padding(
-            top = 16.dp,
-            bottom = 16.dp,
-            start = 8.dp,
-            end = 8.dp
-        )
-    ) {
-        Text(text = "Shot")
-    }
-    Divider(
-        color = Color.Cyan, modifier = Modifier
-            .height(1.dp)
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
             .fillMaxWidth()
-    )
+            .padding(
+                top = 16.dp,
+                bottom = 16.dp,
+                start = 8.dp,
+                end = 8.dp
+            )
+    ) {
+        Text(text = "Shot", style = typography.titleSmall, color = Color(0xFF001833))
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .width(80.dp)
+                    .height(30.dp)
+                    .border(
+                        width = 2.dp,
+                        color = Color(0xFFD8D8D8),
+                        shape = RoundedCornerShape(percent = 50)
+                    )
+            ) {
+                Text(
+                    text = "Single",
+                    textAlign = TextAlign.Center,
+                    style = typography.labelMedium,
+                    color = Color(0xFF001833)
+                )
+            }
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .width(80.dp)
+                    .height(30.dp)
+                    .border(
+                        width = 2.dp,
+                        color = Color(0xFFD8D8D8),
+                        shape = RoundedCornerShape(percent = 50)
+                    )
+            ) {
+                Text(
+                    text = "Double",
+                    textAlign = TextAlign.Center,
+                    style = typography.labelMedium,
+                    color = Color(0xFF001833)
+                )
+            }
+        }
+    }
 }
 
 @Composable
 fun HotColdOption() {
     Divider(
-        color = Color.Cyan, modifier = Modifier
-            .height(1.dp)
+        color = Color(0xFFD8D8D8), modifier = Modifier
+            .height(2.dp)
             .fillMaxWidth()
     )
     Row(
-        modifier = Modifier.padding(
-            top = 16.dp,
-            bottom = 16.dp,
-            start = 8.dp,
-            end = 8.dp
-        )
-    ) {
-        Text(text = "Select")
-    }
-    Divider(
-        color = Color.Cyan, modifier = Modifier
-            .height(1.dp)
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
             .fillMaxWidth()
-    )
+            .padding(
+                top = 16.dp,
+                bottom = 16.dp,
+                start = 8.dp,
+                end = 8.dp
+            )
+    ) {
+        Text(text = "Select", style = typography.titleSmall, color = Color(0xFF001833))
+
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            IconButton(onClick = {}) {
+                Icon(
+                    painterResource(id = R.drawable.standardcup),
+                    contentDescription = null,
+                    tint = Color(0xFF001833)
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    painterResource(id = R.drawable.plasticcup),
+                    contentDescription = null,
+                    tint = Color(0xFF001833)
+                )
+            }
+        }
+    }
 }
 
 @Composable
 fun SizeOption() {
     Divider(
-        color = Color.Cyan, modifier = Modifier
-            .height(1.dp)
+        color = Color(0xFFD8D8D8), modifier = Modifier
+            .height(2.dp)
             .fillMaxWidth()
     )
     Row(
-        modifier = Modifier.padding(
-            top = 16.dp,
-            bottom = 16.dp,
-            start = 8.dp,
-            end = 8.dp
-        )
-    ) {
-        Text(text = "Size")
-    }
-    Divider(
-        color = Color.Cyan, modifier = Modifier
-            .height(1.dp)
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
             .fillMaxWidth()
-    )
+            .padding(
+                top = 16.dp,
+                bottom = 16.dp,
+                start = 8.dp,
+                end = 8.dp
+            )
+    ) {
+        Text(text = "Size", style = typography.titleSmall, color = Color(0xFF001833))
+
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            IconButton(onClick = {}) {
+                Icon(
+                    painterResource(id = R.drawable.smallsize),
+                    contentDescription = null,
+                    tint = Color(0xFF001833)
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    painterResource(id = R.drawable.mediumsize),
+                    contentDescription = null,
+                    tint = Color(0xFF001833)
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    painterResource(id = R.drawable.bigsize),
+                    contentDescription = null,
+                    tint = Color(0xFF001833)
+                )
+            }
+        }
+    }
 }
 
 @Composable
 fun IceOption() {
     Divider(
-        color = Color.Cyan, modifier = Modifier
-            .height(1.dp)
+        color = Color(0xFFD8D8D8), modifier = Modifier
+            .height(2.dp)
             .fillMaxWidth()
     )
     Row(
-        modifier = Modifier.padding(
-            top = 16.dp,
-            bottom = 16.dp,
-            start = 8.dp,
-            end = 8.dp
-        )
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                top = 16.dp,
+                bottom = 16.dp,
+                start = 8.dp,
+                end = 8.dp
+            )
     ) {
-        Text(text = "Ice")
+        Text(text = "Ice", style = typography.titleSmall, color = Color(0xFF001833))
+
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            IconButton(onClick = {}) {
+                Icon(
+                    painterResource(id = R.drawable.ice1),
+                    contentDescription = null,
+                    tint = Color(0xFF001833)
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    painterResource(id = R.drawable.ice2),
+                    contentDescription = null,
+                    tint = Color(0xFF001833)
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    painterResource(id = R.drawable.ice3),
+                    contentDescription = null,
+                    tint = Color(0xFF001833)
+                )
+            }
+        }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun AmountOptionPreview() {
     CoffeeHouseTheme {
-        AmountOption(name = "Americano")
+        val counter = rememberSaveable { mutableStateOf(1) }
+        AmountOption(name = "Americano", counter = counter)
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun ShotOptionPreview() {
     CoffeeHouseTheme {
@@ -142,7 +325,7 @@ fun ShotOptionPreview() {
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun HotColdOptionPreview() {
     CoffeeHouseTheme {
@@ -150,7 +333,7 @@ fun HotColdOptionPreview() {
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun SizeOptionPreview() {
     CoffeeHouseTheme {
@@ -158,7 +341,7 @@ fun SizeOptionPreview() {
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun IceOptionPreview() {
     CoffeeHouseTheme {
